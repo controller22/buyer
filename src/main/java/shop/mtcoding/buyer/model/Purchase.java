cf8fa9e851b9a23f4aa6ce3a10fb8b30646cf1e2
DH <shim5013@naver.com>
2023-01-17 16:49:27 +0900

    레파지토리 설계 완료

--- src/main/java/shop/mtcoding/buyer/model/Purchase.java
+++ src/main/java/shop/mtcoding/buyer/model/Purchase.java
@@ -1,5 +1,16 @@
 package shop.mtcoding.buyer.model;
 
+import java.sql.Timestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Purchase {
+    private int id;
+    private int usrId;
+    private int productId;
+    private Timestamp createdAt;
 
 }
